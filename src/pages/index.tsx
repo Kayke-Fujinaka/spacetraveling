c295b5f65d742df09ad409ebf989af0747af8b6f
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-12-29 06:04:16 -0300

    feat: render condition in See More and Slug to post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import { GetStaticProps } from 'next';
-
+import { useState } from 'react';
+import Header from '../components/Header';
+import Post from '../components/Post';
 import { getPrismicClient } from '../services/prismic';
+import { Main, SeeMore } from '../styles/pages/Home';
+import { formattedDate } from '../utils/dateFormat';
 
-import commonStyles from '../styles/common.module.scss';
-import styles from './home.module.scss';
-
-interface Post {
+interface IPost {
   uid?: string;
   first_publication_date: string | null;
   data: {
@@ -17,20 +18,83 @@ interface Post {
 
 interface PostPagination {
   next_page: string;
-  results: Post[];
+  results: IPost[];
 }
 
 interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps) {
+  const formattedPost = postsPagination.results.map((post: IPost) => {
+    return {
+      ...post,
+      first_publication_date: formattedDate(post.first_publication_date),
+    };
+  });
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+  const [posts, setPosts] = useState<IPost[]>(formattedPost);
+  const [nextPage, setNextPage] = useState(postsPagination.next_page);
+  const [currentPage, setCurrentPage] = useState(1);
 
-//   // TODO
-// };
+  async function handleNextPage(): Promise<void> {
+    if (currentPage !== 1 && nextPage === null) return;
+
+    const postsResults = await fetch(`${nextPage}`).then(response =>
+      response.json()
+    );
+
+    setNextPage(postsResults.next_page);
+    setCurrentPage(postsResults.page);
+
+    const newPosts = postsResults.results.map((post: IPost) => {
+      return {
+        uid: post.uid,
+        first_publication_date: formattedDate(post.first_publication_date),
+        data: {
+          title: post.data.title,
+          subtitle: post.data.subtitle,
+          author: post.data.author,
+        },
+      };
+    });
+
+    setPosts([...posts, ...newPosts]);
+  }
+
+  return (
+    <>
+      <Header />
+      <Main className="wrapper">
+        {posts && posts.map(post => <Post key={post.uid} post={post} />)}
+        {nextPage && (
+          <SeeMore>
+            <span>Carregar mais posts</span>
+          </SeeMore>
+        )}
+      </Main>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient({});
+  const response = await prismic.getByType('post', { pageSize: 1 });
+
+  const posts = response.results.map<IPost>(post => ({
+    uid: post.uid,
+    first_publication_date: post.first_publication_date,
+    data: {
+      title: post.data.title,
+      subtitle: post.data.subtitle,
+      author: post.data.author,
+    },
+  }));
+
+  const postsPagination = {
+    next_page: response.next_page,
+    results: posts,
+  };
+
+  return { props: { postsPagination }, revalidate: 1800 };
+};
