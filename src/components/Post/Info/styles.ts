a7a6f2958f219a69a66837643dc0e253d863f54d
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-01-03 11:33:07 -0300

    style: centralize post info

--- src/components/Post/Info/styles.ts
+++ src/components/Post/Info/styles.ts
@@ -3,7 +3,8 @@ import styled, { css } from 'styled-components';
 export const Info = styled.div`
   ${({ theme }) => css`
     display: flex;
-    align-items: flex-end;
+    align-items: center;
+    justify-content: center;
     gap: ${theme.spaces[2]};
     color: ${theme.colors.info};
   `}
